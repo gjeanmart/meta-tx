3e11c86ce2cfac4a0526e7d77693c4225b57aa3e
gjeanmart <gregoire.jeanmart@consensys.net>
2018-11-27 22:59:55 +0000

    added sokol + dockerize frontend

--- docker/docker-compose.sh
+++ docker/docker-compose.sh
@@ -5,11 +5,11 @@ docker-compose down
 [ $? -eq 0 ] || exit $?; 
 
 echo "Build"
-docker-compose build 
+docker-compose `if [ ! -z $1 ]; then echo "-f docker-compose-$1.yml"; fi` build 
 [ $? -eq 0 ] || exit $?; 
 
 echo "Start"
-docker-compose up 
+docker-compose `if [ ! -z $1 ]; then echo "-f docker-compose-$1.yml"; fi` up 
 [ $? -eq 0 ] || exit $?; 
 
 trap "docker-compose kill" INT
